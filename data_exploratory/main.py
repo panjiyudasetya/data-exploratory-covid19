55c29b41d229e762cd270e3a1e9e1a470d158d6d
panjiyudasetya <panjiyudasetya@gmail.com>
2023-01-19 16:55:44 +0700

    Add skeleton's code to run data exploratory - Add skeleton's code to run Covid-19 data exploratory - Add script to run the data exploratory project

---
+++ data_exploratory/main.py
@@ -0,0 +1,12 @@
+
+class DataExploratory:
+
+    def run(self) -> None:
+        """
+        Runs the COVID-19 data exploratory.
+        """
+        pass
+
+
+if __name__ == '__main__':
+    DataExploratory().run()
